695a5bfc8cb375f739a9797fd0f3b7cf0e7333c7
Nikita Mezhenskyi <nikita.mezhenskyi@gmail.com>
2022-05-15 13:47:42 -0400

    feat(main): shutdown server on SIGINT & SIGTERM

--- cmd/restapi/main.go
+++ cmd/restapi/main.go
@@ -6,22 +6,41 @@
 package main
 
 import (
+	"context"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/nmezhenskyi/go-rest-api-example/internal/webserver"
 )
 
 func main() {
-	if err := run(); err != nil {
-		log.Fatal(err)
-	}
-}
-
-func run() error {
 	server := webserver.NewServer()
 	err := server.PopulateWithData("data/data_sample.json")
 	if err != nil {
-		return err
+		log.Fatalf("Failed to load data sample: %v\n", err)
 	}
-	return server.ListenAndServe("localhost:5000")
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		err := server.ListenAndServe("localhost:5000")
+		if err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Server failed: %v\n", err)
+		}
+	}()
+
+	<-shutdown
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err = server.Shutdown(ctx); err != nil {
+		log.Fatalf("Server shutdown failed: %v\n", err)
+	}
+	log.Println("Server has been shutdown")
 }
